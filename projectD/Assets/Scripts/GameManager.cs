841964f814902e0ee4f33f5c61dc2f5e3c3146aa
Vega0910 <dlalfm09@g.shingu.ac.kr>
2023-11-27 12:28:35 +0900

    Feat"그동안 안올렸던거"
    드래그시 복제 오브젝트 화살표, 사망시 이동처리, 턴넘기기 리스타트

--- projectD/Assets/Scripts/GameManager.cs
+++ projectD/Assets/Scripts/GameManager.cs
@@ -4,73 +4,87 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private List<Transform> spawnPoints = new List<Transform>();
-    private int spawnIndex = 0;
+    public List<Transform> pointsGroup1 = new List<Transform>();  // 플레이어1의 스폰 포인트 리스트
+    public List<Transform> pointsGroup2 = new List<Transform>();  // 플레이어2의 스폰 포인트 리스트
+    public List<GameObject> spawnedMarges = new List<GameObject>();
 
+    private List<GameObject> margeList = new List<GameObject>(); // Marge 오브젝트를 관리할 리스트
+    private List<Transform> currentPoints; // 현재 사용 중인 스폰 포인트 그룹
+
+    // Start is called before the first frame update
     void Start()
     {
-        // SpawnPointGroup 게임오브젝트의 Transform 컴포넌트 추출
-        Transform spawnPointGroup = GameObject.Find("SpawnPointGroup")?.transform;
+        // 자식 포인트들을 자동으로 등록
+        Transform spawnPointGroup1 = GameObject.Find("SpawnPointGroup1")?.transform;
+        Transform spawnPointGroup2 = GameObject.Find("SpawnPointGroup2")?.transform;
 
+        if (spawnPointGroup1 != null && spawnPointGroup2 != null)
+        {
+            RegisterSpawnPoints(spawnPointGroup1, pointsGroup1);
+            RegisterSpawnPoints(spawnPointGroup2, pointsGroup2);
+
+            // 초기에는 Player1의 스폰 포인트 그룹을 선택
+            currentPoints = pointsGroup1;
+        }
+        else
+        {
+            Debug.LogError("스폰 포인트 그룹을 찾을 수 없어요!");
+        }
+    }
+
+    private void RegisterSpawnPoints(Transform spawnPointGroup, List<Transform> spawnPointsList)
+    {
         if (spawnPointGroup != null)
         {
-            // SpawnPointGroup 하위에 있는 모든 차일드 게임오브젝트의 Transform 컴포넌트 추출
             foreach (Transform point in spawnPointGroup)
             {
-                spawnPoints.Add(point);
+                spawnPointsList.Add(point);
             }
         }
         else
         {
-            Debug.LogError("SpawnPointGroup not found!");
+            Debug.LogError("스폰 포인트 그룹이 없어요!");
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    public void AddSpawnedMargeToList(GameObject marge)
     {
-        // 트리거를 통과한 오브젝트가 "Player" 테그를 가졌는지 확인
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Player triggered!");
-
-            // 트리거의 Collider를 비활성화
-            GetComponent<Collider>().enabled = false;
-
-            // 플레이어 스폰
-            SpawnPlayer();
-        }
+        spawnedMarges.Add(marge);
     }
 
-    private void OnTriggerExit(Collider other)
+    // Marge 오브젝트를 리스트에 추가하는 함수
+    public void AddMargeToList(GameObject marge)
     {
-        // 플레이어가 트리거를 빠져나가면 트리거를 다시 활성화
-        if (other.CompareTag("Player"))
-        {
-            // 트리거의 Collider를 활성화
-            GetComponent<Collider>().enabled = true;
-        }
+        margeList.Add(marge);
     }
 
-    void SpawnPlayer()
+    public Vector3 GetNextSpawnPoint(List<Transform> spawnPoints)
     {
-        if (spawnPoints.Count == 0)
-        {
-            Debug.LogError("Spawn points list is empty!");
-            return;
-        }
-
-        // 플레이어 생성
-        GameObject player = Instantiate(Resources.Load<GameObject>("Prefabs/PlayerPrefab"), GetNextSpawnPoint(), Quaternion.identity);
-
-        // 플레이어의 Marge 오브젝트 생성 및 연결
-        GameObject marge = Instantiate(Resources.Load<GameObject>("Prefabs/MargePrefab"), player.transform.position, player.transform.rotation);
-        marge.transform.parent = player.transform;
-    }
-
-    Vector3 GetNextSpawnPoint()
-    {
-        int currentIndex = spawnIndex % spawnPoints.Count;
-        spawnIndex++;
+        int currentIndex = spawnedMarges.Count % spawnPoints.Count;
         return spawnPoints[currentIndex].position;
+    }
+
+    // 새로운 스폰 메서드 추가
+    public void SpawnMarge(GameObject margePrefab, string playerTag)
+    {
+        // 스폰 포인트 그룹 선택
+        List<Transform> spawnPoints = (playerTag == "Player1") ? pointsGroup1 : pointsGroup2;
+
+        // 스폰 포인트 그룹에서 다음 스폰 위치 가져오기
+        Vector3 spawnPosition = GetNextSpawnPoint(spawnPoints);
+
+        // Marge 프리팹 생성 및 위치 설정
+        GameObject marge = Instantiate(margePrefab, spawnPosition, Quaternion.identity);
+
+        // Rigidbody 컴포넌트 활성화
+        Rigidbody margeRigidbody = marge.GetComponent<Rigidbody>();
+        if (margeRigidbody != null)
+        {
+            margeRigidbody.isKinematic = true;
+        }
+
+        // 게임 매니저에 Marge 추가
+        AddMargeToList(marge);
+        AddSpawnedMargeToList(marge);
     }
 }
